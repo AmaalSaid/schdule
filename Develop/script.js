2c8cf800e888ba21649af4c3a79c636764dceeac
AmaalSaid <amaal_1996@hotmail.co.uk>
2022-08-04 23:37:49 +0200

    save and retrive

--- Develop/script.js
+++ Develop/script.js
@@ -3,9 +3,16 @@ var update= function() {
     $("#currentDay").text( moment().format("dddd, MMMM Do, YYYY"))
 };
 setInterval (update, 1000);
-
+//Save
 $(".saveBtn").on("click",function(){
     var userinput = $(this).siblings("textarea").val()
     var timeblock = $(this).attr("id")
     console.log(userinput,timeblock)
+    localStorage.setItem(timeblock,userinput)
 })
+
+//retrive
+for(let i=9;i<18;i++){
+    var userStoredPlan = localStorage.getItem(i)
+    $("#"+i).siblings("textarea").val(userStoredPlan)
+}
