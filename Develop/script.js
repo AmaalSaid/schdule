b1fa114f6c3113921666f9889eb13eb32c0da2d1
AmaalSaid <amaal_1996@hotmail.co.uk>
2022-08-04 23:29:09 +0200

    onclick

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
-const currentDate = () => {
-    const dateTime = $("#currentDay")
-    const displayNow = momnet().format("dddd, MMMM Do, YYYY")
-    dateTime.text(displayNow)
-}
+var currentHour = moment().hours();
+var update= function() {
+    $("#currentDay").text( moment().format("dddd, MMMM Do, YYYY"))
+};
+setInterval (update, 1000);
+
+$(".saveBtn").on("click",function(){
+    var userinput = $(this).siblings("textarea").val()
+    var timeblock = $(this).attr("id")
+    console.log(userinput,timeblock)
+})
